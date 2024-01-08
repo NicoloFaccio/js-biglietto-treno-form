f396fd82bb247c6e24969aef1fedbc203d202c93
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-01-08 15:27:47 +0100

    worked on the variables and console log

---
+++ assets/javascript/main.js
@@ -0,0 +1,15 @@
+//INTIALISATION OF THE VARIABLES
+let nomeCogmome= document.getElementById(nameSurname);
+let numeroKilometri= document.getElementById(numberKm);
+let etaPasseggero= document.getElementById(numberAge);
+const bottoneHtml= document.getElementById(button);
+let biglietto= document.getElementById(ticket);
+
+//PART ABOUT THE CONSOLE.LOG
+
+console.log("Nome del passeggero:", nameSurname);
+console.log("N° di chilometri da percorrere:", numberKm);
+console.log("Età del passeggero:", numberAge);
+
+
+
