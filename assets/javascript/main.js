991e3251cc0389b4690863e98337a073a2daa30b
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-01-09 20:26:02 +0100

    feat:working on the functions on the click of the button

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -13,6 +13,26 @@ const numeroCarrozza= document.getElementById(numberCoach);
 const codiceCp= document.getElementById(codeCp);
 const prezzoBiglietto= document.getElementById(priceTicket);
 
+//PART ABOUT THE FUNCTIONS
+    sottotitolo.style.display = "none"
+    biglietto.style.display = "none" 
 
+bottone.addEventListener("click", function() {
+    sottotitolo.style.display= "block"
+    biglietto.style.display= "block" 
 
+    let codiceCarozza= RandomNumber (0,10)
+    let cpCodiceHtml= RandomNumber (0,10000)
+
+    numeroCarrozza.innerHTML = codiceCarozza
+    codiceCp.innerHTML = cpCodiceHtml
+     
+    nomePasseggero.innerHTML = nomeCognome.value
+
+    if (etaPersona=== "minorenne")
+})
+
+function RandomNumber( min , max ) {
+    return Math.floor( Math.random()*(max-min + 1) + 1 )
+}
 
