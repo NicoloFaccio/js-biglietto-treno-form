64637bea48380bc8b35bd9a8c2b2fe4e5d14e3d5
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-01-10 17:28:53 +0100

    fixed the variables

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,17 +1,17 @@
 //INTIALISATION OF THE VARIABLES
-const nomeCognome= document.getElementById(nameSurname);
-const chilometri= document.getElementById(Kilometers);
-const etaPersona= document.getElementById(agePerson);
-const bottone= document.getElementById(button);
+const nomeCognome= document.getElementById("nameSurname");
+const chilometri= document.getElementById("Kilometers");
+const etaPersona= document.getElementById("agePerson");
+const bottone= document.getElementById("button");
 
-const sottotitolo= document.getElementById(subTitle);
-const biglietto= document.getElementById(secondCard);
+const sottotitolo= document.getElementById("subTitle");
+const biglietto= document.getElementById("secondCard");
 
-const nomePasseggero= document.getElementById(namePassenger);
-const offertaBiglietto= document.getElementById(ticketOffert);
-const numeroCarrozza= document.getElementById(numberCoach);
-const codiceCp= document.getElementById(codeCp);
-const prezzoBiglietto= document.getElementById(priceTicket);
+const nomePasseggero= document.getElementById("namePassenger");
+const offertaBiglietto= document.getElementById("ticketOffert");
+const numeroCarrozza= document.getElementById("numberCoach");
+const codiceCp= document.getElementById("codeCp");
+const prezzoBiglietto= document.getElementById("priceTicket");
 
 //PART ABOUT THE FUNCTIONS
     sottotitolo.style.display = "none"
